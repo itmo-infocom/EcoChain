f39a06799df6cb5a22cd95844bdb1a1c3050a271
Manish Lamichhane <manishlamichhane@gmail.com>
2016-12-06 19:41:29 +0100

    MQTT publish and subscribe working for android

--- DApp/sgb-model-android-app/www/js/index.js
+++ DApp/sgb-model-android-app/www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ var connect = false;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,41 +35,152 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        cordova.plugins.CordovaMqTTPlugin.connect({
+                            url: 'tcp://192.168.0.105', //does not support mqtt://<ip>, strictly tcp://<ip> should be written
+                            port:'1883',
+                            
+                            success:function(s){
+                                connect = true;
+                                /*alert('MQTT Broker connected successfully!');*/
+                                document.getElementById("log").innerHTML = 'MQTT Broker connected successfully!';
+
+                                cordova.plugins.CordovaMqTTPlugin.subscribe({
+                                    topic:"AUTH_USER_REPLY",
+                                    qos:0,
+                                    success:function(s){
+                                      
+                                      document.addEventListener("AUTH_USER_REPLY",function (e) {
+                                          e.preventDefault();
+                                          document.getElementById("log").innerHTML += "--> Payload for"+e.topic+" topic: "+e.payload+"<br>"
+                                          alert(e.payload);
+                                      },false);
+
+                                      cordova.plugins.CordovaMqTTPlugin.listen("AUTH_USER_REPLY",function (payload,params,topic,topic_pattern) {
+                                          //params will be an empty object if topic pattern is NOT used. 
+                                          alert("Reply: "+payload);
+                                          document.getElementById("log").innerHTML += "--> Payload for"+topic+" topic: "+JSON.stringify(payload)+"<br>"
+                                      })
+                                    },
+                                    error:function(e){
+                                      
+                                      alert("err!! something is wrong. check the console")
+                                      console.log(e);
+                                    }
+                                  });//subscribe ends here
+                            },
+                            error: function(s){
+                                alert("err!! something is wrong. check the console")
+                                console.log(e);
+                            }
+                        });
         
 
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        var scanButton = document.getElementById('scanner');
+        scanButton.addEventListener('click',function(){
 
-        console.log('Received Event: ' + id);
+            cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    if(!result.cancelled){
+                       // In this case we only want to process QR Codes
+                       if(result.format == "QR_CODE"){
+                            var decoded_qr_value = result.text;
+
+                            if(!connect){
+
+                                alert('Could not publish as there is no connection to broker');
+
+                            }else{
+
+                            cordova.plugins.CordovaMqTTPlugin.publish({
+                                  topic:"AUTH_USER",
+                                  payload: decoded_qr_value,
+                                  qos:0,
+                                  retain:false,
+                                  success:function(s){
+                                    document.getElementById("log").innerHTML += "--> Success: you have published to the topic, AUTH_USER<br>";
+                                  },
+                                  error:function(e){
+                                    document.getElementById("log").innerHTML += "--> Error: something is wrong, "+e+"<br>";
+                                    //alert("err!! something is wrong. check the console")
+                                    console.log(e);
+                                  }
+                            });
+                        }
+                    }
+                }
+            });
+
+        },false);
     }
 
 };
 
 
-function scan(){
+/*function scan(){
 
     cordova.plugins.barcodeScanner.scan(
         function (result) {
             if(!result.cancelled){
                    // In this case we only want to process QR Codes
                    if(result.format == "QR_CODE"){
-                        var value = result.text;
+                        var decoded_qr_value = result.text;
                         // This is the retrieved content of the qr code
-                        /*alert(value);*/
+                        /*alert(value);
                         cordova.plugins.CordovaMqTTPlugin.connect({
-                            url: 'mqtt://192.168.0.105', //a public broker used for testing purposes only. Try using a self hosted broker for production.
+                            url: 'tcp://192.168.0.105', //does not support mqtt://<ip>, strictly tcp://<ip> should be written
                             port:'1883',
-                            //clientId:document.getElementById("clientId").value,
+                            
                             success:function(s){
                                 connect = true;
-                                alert('MQTT Broker connected successfully!');
+                                /*alert('MQTT Broker connected successfully!');
+                                document.getElementById("log").innerHTML = 'MQTT Broker connected successfully!';
+
+                                //subscribe to edge-hub reply for the published decoded_qr_code below
+
+                                cordova.plugins.CordovaMqTTPlugin.subscribe({
+                                    topic:"AUTH_USER_REPLY",
+                                    qos:0,
+                                    success:function(s){
+                                      
+                                      /*document.addEventListener("AUTH_USER_REPLY",function (e) {
+                                          e.preventDefault();
+                                          document.getElementById("log").innerHTML += "--> Payload for"+e.topic+" topic: "+e.payload+"<br>"
+                                          alert(e.payload);
+                                      },false);
+
+                                      cordova.plugins.CordovaMqTTPlugin.listen("AUTH_USER_REPLY",function (payload,params,topic,topic_pattern) {
+                                          //params will be an empty object if topic pattern is NOT used. 
+                                          alert("Reply: "+payload);
+                                          document.getElementById("log").innerHTML += "--> Payload for"+topic+" topic: "+JSON.stringify(payload)+"<br>"
+                                      })
+                                    },
+                                    error:function(e){
+                                      //document.getElementById("activity").innerHTML += "--> Error: something is wrong when subscribing to this topic, "+e+"<br>";
+                                      //document.getElementById("subscribe").style.display = "block";
+                                      //document.getElementById("unsubscribe").style.display = "none";
+                                      alert("err!! something is wrong. check the console")
+                                      console.log(e);
+                                    }
+                                  });
+
+                                cordova.plugins.CordovaMqTTPlugin.publish({
+                                  topic:"AUTH_USER",
+                                  payload: decoded_qr_value,
+                                  qos:0,
+                                  retain:false,
+                                  success:function(s){
+                                    document.getElementById("log").innerHTML += "--> Success: you have published to the topic, AUTH_USER<br>";
+                                  },
+                                  error:function(e){
+                                    document.getElementById("log").innerHTML += "--> Error: something is wrong, "+e+"<br>";
+                                    //alert("err!! something is wrong. check the console")
+                                    console.log(e);
+                                  }
+                                });
                             },
                             error:function(e){
                                 connect = false;
@@ -93,3 +205,4 @@ function scan(){
     );
 
 }
+*/
