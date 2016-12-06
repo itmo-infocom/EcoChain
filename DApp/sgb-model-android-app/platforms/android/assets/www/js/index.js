238a81010853a0cd4229e3b23856614b5f02c30d
Manish Lamichhane <manishlamichhane@gmail.com>
2016-12-06 16:42:09 +0100

    Development Phase: Android app with QR scanner and MQTT that models Smart Garbage Bins

---
+++ DApp/sgb-model-android-app/platforms/android/assets/www/js/index.js
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        
+
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+
+};
+
+
+function scan(){
+
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if(!result.cancelled){
+                   // In this case we only want to process QR Codes
+                   if(result.format == "QR_CODE"){
+                        var value = result.text;
+                        // This is the retrieved content of the qr code
+                        alert(value);
+                        cordova.plugins.CordovaMqTTPlugin.connect({
+                            url: 'mqtt://192.168.0.105', //a public broker used for testing purposes only. Try using a self hosted broker for production.
+                            port:'1883',
+                            //clientId:document.getElementById("clientId").value,
+                            success:function(s){
+                                connect = true;
+                                alert('MQTT Broker connected successfully!');
+                            },
+                            error:function(e){
+                                connect = false;
+                                alert("err!! something is wrong. check the console")
+                                console.log(e);
+                            },
+                            onConnectionLost:function (){
+                              connect = false;
+                                alert('connection lost with mqtt broker');
+                            }
+                        })
+                   }else{
+                      alert("Sorry, only qr codes this time ;)");
+                   }
+            }else{
+              alert("The user has dismissed the scan");
+            }
+         },
+         function (error) {
+              alert("An error ocurred: " + error);
+         }
+    );
+
+}
--- DApp/waste-management-edge-hub/routes/mqtt-edge-hub.js
+++ DApp/waste-management-edge-hub/routes/mqtt-edge-hub.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://localhost:1883');
-var Web3 = require('web3');
-var awsIP = "http://54.214.108.183:5678";
+/*var Web3 = require('web3');
+var awsIP = "http://54.203.224.250";
 var awsPort = "5678";
 var blockChainRpcUrl = awsIP+":"+awsPort;
-var web3 = new Web3(new Web3.providers.HttpProvider(blockChainRpcUrl));
+var web3 = new Web3(new Web3.providers.HttpProvider(blockChainRpcUrl));*/
 
 
 router.get('/', function(req, res) {
@@ -22,11 +22,11 @@ router.get('/', function(req, res) {
 	    
 	    var account = message.toString();
 	    //rpc to remote block chain made here
-	    var balance = web3.eth.getBalance(account);
+	   /* var balance = web3.eth.getBalance(account);*/
 	    var response = "Open : false";
-	    if(balance >= 1){
+	    /*if(balance >= 1){
 	    	response = "Open : true";
-	    }
+	    }*/
 
 	    client.publish('AUTH_USER_REPLY',response);
 
