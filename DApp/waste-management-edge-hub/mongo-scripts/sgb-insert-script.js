661228ddb3f46c5cef190248c5bf020cf75ae595
Manish Lamichhane <manishlamichhane@gmail.com>
2017-02-28 23:39:23 +0300

    SGB table created in MongoDB. Dummy SGB Data Inserted.Documented

---
+++ DApp/waste-management-edge-hub/mongo-scripts/sgb-insert-script.js
@@ -0,0 +1,59 @@
+db.sgb_location.insert(
+						[
+							{
+								"location" : [60.0120326,30.2857044],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Municipal"
+							},
+							{
+								"location" : [59.9445047,30.2929776],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Private"
+							},
+							{
+								"location" : [59.9399965,30.2963032],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Municipal",
+							},
+							{
+								"location" : [59.9600023,30.2340458],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Private"
+							},
+							{
+								"location" : [59.7315789,30.2578202],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Municipal"
+							},
+							{
+								"location" : [60.1400312,29.9646915],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Private"
+							},
+							{
+								"location" : [59.9788741,30.2938835],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Municipal"
+							},
+							{
+								"location" : [59.9740334,30.3793894],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Private"
+							},
+							{
+								"location" : [59.9702989,30.416928],
+								"installed_date" : new Date().toLocaleFormat("%Y-%b-%d"),
+								"minimum_balance_required" : "1",
+								"type" : "Municipal"
+							}
+
+						]
+					);
